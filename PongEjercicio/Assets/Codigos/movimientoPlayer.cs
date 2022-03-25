0ca4b426913cc8e99a7dc213f94b7a41e57dbb79
Loren <lorenquiroz@gmail.com>
2022-03-24 20:55:38 -0500

    Movimiento_Pelota
    Movimiento de la pelota

---
+++ PongEjercicio/Assets/Codigos/movimientoPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoPlayer : MonoBehaviour
+{
+    public Vector2 DireccionMovimiento;
+    private float velocidadDeMovimiento;
+    void Update()
+    {
+        
+    }
+}
